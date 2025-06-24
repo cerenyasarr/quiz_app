e7dc81780acee8561820578f75c42a526ce8645a
Ceren Yaşar <cerenysr1@gmail.com>
2025-06-24 23:16:23 +0300

    fix button

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -6,10 +6,32 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Center(
-        child: Column(
-      children: [
-        Image.asset('assets/images/quiz-logo.png'),
-      ],
-    ));
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 219, 193, 235),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
   }
 }
