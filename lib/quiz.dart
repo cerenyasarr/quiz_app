fc0ffe01b4073850a1ac99217e8c925364a0f2ea
Ceren Yaşar <cerenysr1@gmail.com>
2025-06-25 15:15:31 +0300

    feat: add screen switching logic between StartScreen and QuestionsScreen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -10,6 +12,14 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -25,7 +35,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
