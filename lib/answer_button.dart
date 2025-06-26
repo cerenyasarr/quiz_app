4f3954a18faeea19069292e499cf537a9407ddcc
Ceren Yaşar <cerenysr1@gmail.com>
2025-06-26 23:10:29 +0300

    style: customize answer button appearance

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  AnswerButton({
+  const AnswerButton({
     super.key,
     required this.answerText,
     required this.onTap,
@@ -14,7 +14,17 @@ class AnswerButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onTap,
-      style: ElevatedButton.styleFrom(),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
+        backgroundColor: const Color.fromARGB(255, 39, 18, 43),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
       child: const Text('Answer 1'),
     );
   }
