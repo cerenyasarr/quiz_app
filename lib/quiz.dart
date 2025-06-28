6fd9bfaaa67848e40f69ecb48da0e9615233550a
Ceren Yaşar <cerenysr1@gmail.com>
2025-06-29 00:06:49 +0300

    refactor: pass onSelectAnswer callback to QuestionsScreen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -30,7 +30,9 @@ class _QuizState extends State<Quiz> {
     Widget screenWidget = StartScreen(switchScreen);
 
     if (activeScreen == 'questions-screen') {
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
     }
 
     return MaterialApp(
